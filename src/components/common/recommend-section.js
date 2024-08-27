5f4ea8abd2536ee912331aa75bfca8a56332663f
nishantmann19 <95399435+nishantmann19@users.noreply.github.com>
2024-08-27 14:32:45 +0530

    unwanted component

--- src/components/common/recommend-section.js
+++ src/components/common/recommend-section.js
@@ -2,7 +2,6 @@ import React, { useRef } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
-import { IMAGE_TYPES } from "../../defaultImage/imges";
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
